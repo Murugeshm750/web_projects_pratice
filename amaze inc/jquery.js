df637f4414411093a7b2e66bc6e4fbee2caec1fb
Murugesh M <115576038+Murugeshm750@users.noreply.github.com>
2023-10-10 23:13:20 +0530

    Add files via upload

---
+++ amaze inc/jquery.js
@@ -0,0 +1,43 @@
+image img {
+    width:100%;
+    border-radius: 10px 10px 0px 0px ;
+}
+.img-logo img {
+    width: 100px;
+}
+.top-style {
+    text-align: start;
+}
+.top-style p {
+    margin-left: 180px;
+    font-size: 25px;
+    color: #A1BF36;
+}
+.top-style p::after {
+    content: "___";
+    margin-top: -10px;
+    margin-left: 5px;
+    position: absolute;
+}
+.top-style h1 {
+    width: 70%;
+    margin-left: 200px;
+    font-size: 25px;
+    font-weight: 1000;
+    font-size: 35px;
+}
+.set-box {
+    display: flex;
+    position: absolute;
+    width: 100%;
+    justify-content:center;
+    gap: 30px;
+    height: 60vh;
+    padding-top: 50px;
+}
+.set-box .set {
+    background-color: #FAFAFA;
+    width: 350px;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+}
